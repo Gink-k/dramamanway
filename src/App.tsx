1289b95c27fac18f424aa6c88f937a587e89cb50
Gink-k <gink.official01@gmail.com>
2024-05-09 22:56:14 +0300

    add logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import './App.css';
+import './styles.module.scss';
 import Dramamanway from './pages/dramamanway';
-
+import s from './styles.module.scss';
+import { CatLogo } from './ui/icons';
 function App() {
-    // const fetchPage = useStore(state => state.fetchDramamanwayPosts);
-
     return (
-        <div className="App">
+        <div className={s.app}>
+            <div className={s.head}>
+                <p className={s.logo}>
+                    <CatLogo className={s.icon} size={64} fill={'orange'} />
+                    По пути дорамщика
+                </p>
+            </div>
             <Dramamanway />
         </div>
     );
--- src/pages/dramamanway/index.tsx
+++ src/pages/dramamanway/index.tsx
@@ -1,9 +1,9 @@
 import React, { FC } from 'react';
 import { Table } from './table';
-import s from './styles.module.scss';
+
 const Dramamanway: FC = () => {
     return (
-        <div className={s.page}>
+        <div>
             <Table />
         </div>
     );
