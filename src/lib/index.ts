f65bf67074bfde987960c95efaf3fc2d424eb154
Gink-k <gink.official01@gmail.com>
2024-05-12 21:20:06 +0300

    deploy

--- src/lib/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './dramamanway-post';
-export * from './win1251-to-utf8';
-export * from './round2';
-export * from './score';
-export * from './to-number';
--- src/lib/to-number.ts
+++
@@ -1,5 +0,0 @@
-export const toNumber = (raw: string, defaultValue = -1) => {
-    const res = Number(raw);
-
-    return Number.isNaN(res) ? defaultValue : res;
-};
